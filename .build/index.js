b4d060af60b86b42f9a8faaaa7762aaf7e5758a9
Marina Almeida <marinabrito.ma@gmail.com>
2022-09-15 09:52:40 +0000

    create generics

--- .build/index.js
+++ .build/index.js
@@ -1,4 +1,19 @@
+const mustBeEqual = (x, y) => {
+  if (x !== y)
+    throw new Error(`${x} must be equal to ${y}`);
+  console.log(`${x} is equal to ${y}`);
+};
+mustBeEqual(123, 123);
+mustBeEqual(123, "abc");
 const numMustBeEqual = (x, y) => {
+  if (x !== y)
+    throw new Error(`${x} must be equal to ${y}`);
+};
+const strMustBeEqual = (x, y) => {
+  if (x !== y)
+    throw new Error(`${x} must be equal to ${y}`);
+};
+const anyMustBeEqual = (x, y) => {
   if (x !== y)
     throw new Error(`${x} must be equal to ${y}`);
 };
--- index.ts
+++ index.ts
@@ -1,5 +1,27 @@
 // Create a function to assert that two numbers are the same
 // if not, throw an Error
+
+// Generics Function 
+const mustBeEqual = <T>(x: T, y: T) => { // T means type generic
+  if(x !== y) throw new Error (`${x} must be equal to ${y}`)
+  console.log(`${x} is equal to ${y}`)
+}
+
+mustBeEqual(123, 123)
+mustBeEqual(123, 'abc')
+
+// dont create many functions with the same purpose
 const numMustBeEqual = (x: number, y: number) => {
   if(x !== y) throw new Error (`${x} must be equal to ${y}`)
 }
+
+const strMustBeEqual = (x: string, y: string) => {
+  if(x !== y) throw new Error (`${x} must be equal to ${y}`)
+}
+
+// this will compare the type
+const anyMustBeEqual = (x: any, y: any) => {
+  if(x !== y) throw new Error (`${x} must be equal to ${y}`)
+}
+
+// anyMustBeEqual(123, 'abc')
